eacb88e9337b7694832218dd75a1ba26d7bc2be0
rjie <rjie@node3000.inband>
2026-03-09 16:49:35 -0400

    bakup0309

---
+++ reasoning/grandriver.sh
@@ -0,0 +1,30 @@
+JOB_ID=`cat job_id.log`
+echo $JOB_ID
+NEXT_JOB_ID=`expr $JOB_ID + 1`
+echo $NEXT_JOB_ID > job_id.log
+
+GPU_ID="0"
+
+MY_CMD="python aime24.py --model_name deepseek-ai/DeepSeek-R1-0528-Qwen3-8B --data_path HuggingFaceH4/aime_2024 --out ./results/aime24_results.jsonl --n 64"
+
+MY_ROOT_PATH=`pwd`
+
+echo "cd ${MY_ROOT_PATH}" > ./cmd/cmd_${JOB_ID}.sh
+echo "MY_CMD=\"${MY_CMD} \"" >> ./cmd/cmd_${JOB_ID}.sh
+echo "CUDA_VISIBLE_DEVICES='${GPU_ID}' \${MY_CMD}" >> ./cmd/cmd_${JOB_ID}.sh
+echo "if [ \$? -eq 0 ];then" >> ./cmd/cmd_${JOB_ID}.sh
+echo "echo -e \"grandriver JobID:${JOB_ID} \\\n Python_command: \\\n ${MY_CMD} \\\n \" | mail -s \"[Done] grandriver ${SLURM_JOB_ID}\" renjie2179@outlook.com" >> ./cmd/cmd_${JOB_ID}.sh
+echo "else" >> ./cmd/cmd_${JOB_ID}.sh
+echo "echo -e \"grandriver JobID:${JOB_ID} \\\n Python_command: \\\n ${MY_CMD} \\\n \" | mail -s \"[Fail] grandriver ${SLURM_JOB_ID}\" renjie2179@outlook.com" >> ./cmd/cmd_${JOB_ID}.sh
+echo "fi" >> ./cmd/cmd_${JOB_ID}.sh
+
+nohup sh ./cmd/cmd_${JOB_ID}.sh >./logfile/${JOB_ID}.log 2>./logfile/${JOB_ID}.err &
+
+echo "GPU_ID=${GPU_ID}"
+echo $MY_CMD
+
+date >>./history_job.log
+echo ${JOB_ID}>>./history_job.log
+echo "GPU_ID=${GPU_ID}">>./history_job.log
+echo ${MY_CMD}>>./history_job.log
+echo "---------------------------------------------------------------" >>./history_job.log
--- reasoning/local_cmd.sh
+++ reasoning/local_cmd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-MY_CMD="python aime24.py --model_name deepseek-ai/DeepSeek-R1-0528-Qwen3-8B --data_path HuggingFaceH4/aime_2024 --out ./results/aime24_results.jsonl --n 4"
+MY_CMD="python aime24.py --model_name deepseek-ai/DeepSeek-R1-0528-Qwen3-8B --data_path HuggingFaceH4/aime_2024 --out ./results/aime24_results.jsonl --n 64"
 
 echo $MY_CMD
 echo ${MY_CMD}>>local_history.log
